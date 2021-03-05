a153c64e7c80ce25484e126614dacb370ca58c1b
Stéphane Conq <conqstephane@gmail.com>
2021-03-05 22:18:44 +0100

    service worker improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
 import {LOCALE_ID, NgModule} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 import {MaterialModule} from './material/material.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MyStocksComponent } from './components/my-stocks/my-stocks.component';
-import { MenuComponent } from './bottomsheets/menu/menu.component';
+import {MyStocksComponent} from './components/my-stocks/my-stocks.component';
+import {MenuComponent} from './bottomsheets/menu/menu.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {ProdInterceptor} from './interceptors/prod.interceptor';
 import {LocalInterceptor} from './interceptors/local.interceptor';
-import { SearchComponent } from './components/search/search.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { StockInfoComponent } from './components/stock-info/stock-info.component';
-import { PalmaresComponent } from './components/palmares/palmares.component';
-import { PalmaresDividendsComponent } from './components/palmares-dividends/palmares-dividends.component';
+import {SearchComponent} from './components/search/search.component';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
+import {StockInfoComponent} from './components/stock-info/stock-info.component';
+import {PalmaresComponent} from './components/palmares/palmares.component';
+import {PalmaresDividendsComponent} from './components/palmares-dividends/palmares-dividends.component';
 import {ChartsModule} from 'ng2-charts';
-import { NewsComponent } from './components/news/news.component';
-import { NewsListComponent } from './components/news/news-list/news-list.component';
+import {NewsComponent} from './components/news/news.component';
+import {NewsListComponent} from './components/news/news-list/news-list.component';
 import {registerLocaleData} from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
-import { HandleFavoriteComponent } from './bottomsheets/handle-favorite/handle-favorite.component';
+import {HandleFavoriteComponent} from './bottomsheets/handle-favorite/handle-favorite.component';
 
 registerLocaleData(localeFr, 'fr');
 
@@ -51,7 +51,10 @@ registerLocaleData(localeFr, 'fr');
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      registrationStrategy: 'registerImmediately'
+    }),
   ],
   providers: [
     {
@@ -59,8 +62,9 @@ registerLocaleData(localeFr, 'fr');
       useClass: environment.production ? ProdInterceptor : LocalInterceptor,
       multi: true
     },
-    { provide: LOCALE_ID, useValue: 'fr' }
+    {provide: LOCALE_ID, useValue: 'fr'}
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
